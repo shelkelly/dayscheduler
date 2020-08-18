a6fc24edd84c0ba2ec13fbb1d7413f7b3c2e5f76
shelkelly <67724674+shelkelly@users.noreply.github.com>
2020-08-17 23:39:24 -0400

    second commit

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 var currentDay = document.getElementById("#currentDay");
 var timeblockContainer = document.getElementById("#new-todos");
 var hourList = document.getElementById("#hour-list")
-var businessHours = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
 var userInput = document.getElementById("userInput")
+
+
+const businessHours = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
+businessHours.forEach((o,i)=>o.id=i+1);
+console.log(businessHours);
+
 
 // renders hours, textbox and save buttons.
 renderHours();
 
+
+
 function renderHours() {
+
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+
     hourList.innerHTML = "";
     for (var i = 0; i < businessHours.length; i++) {
         var hours = businessHours[i];
@@ -19,12 +29,16 @@ function renderHours() {
         textarea.setAttribute('id', 'userInput');
 
         var button = document.createElement("button");
+        button.setAttribute("data", this);
         button.setAttribute("id", "save");
         button.textContent = "Save";
+        console.log(button);
 
         li.appendChild(textarea);
         li.appendChild(button);
         hourList.appendChild(li);
+
+        
     }
 
         //on clicking save button, then user input should persist
@@ -44,6 +58,29 @@ function renderHours() {
 
     })
 
-
-
 };
+
+function hourTracker() {
+    var currentHour = moment().hour();
+
+    $("li").each(function () {
+        var hourBlock = parseInt($(this).attr("id").split(businessHours)[1]);
+        console.log( blockHour, currentHour)
+
+        if (hourBlock < currentHour) {
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+        }
+        else if (hourBlock === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+}
